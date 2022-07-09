6e4ca6585cf3de81b97d1b69b5b029678bcbe424
AleksandrDruk <sashamironov25650v@gmail.com>
2022-07-09 21:38:07 +0300

    1
    1

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
-const nav = document.querySelector('#nav');
-const navBtn = document.querySelector('#nav-btn');
-const navBtnImg = document.querySelector('#nav-btn-img');
+// const nav = document.querySelector('#nav');
+// const navBtn = document.querySelector('#nav-btn');
+// const navBtnImg = document.querySelector('#nav-btn-img');
 
-navBtn.onclick = () => {
-    if (nav.classList.toggle('open')) {
-        navBtnImg.src = "./img/icons/nav-close.svg";
-    } else {
-        navBtnImg.src = './img/icons/nav-open.svg';
-    }
-}
+// navBtn.onclick = () => {
+//     if (nav.classList.toggle('open')) {
+//         navBtnImg.src = "./img/icons/nav-close.svg";
+//     } else {
+//         navBtnImg.src = './img/icons/nav-open.svg';
+//     }
+// }
 
 AOS.init({
     once: true
